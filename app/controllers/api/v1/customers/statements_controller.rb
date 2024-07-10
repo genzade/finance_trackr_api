cb71858fc1d1bb6a5ba22ef9e6ae4c7fe4c589d8
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-07-10 22:07:28 +0200

    Rename statement controller
    should be `StatementsController` not `StatementController`

---
+++ app/controllers/api/v1/customers/statements_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Customers
+      class StatementsController < ApplicationController
+
+        def show
+        end
+
+      end
+    end
+  end
+end
