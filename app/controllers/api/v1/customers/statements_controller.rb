3729f22e69839175f63955782e80f7960de5f167
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-07-12 00:14:56 +0200

    Statement endpoint returns useful information for customer
    All specs passing

--- app/controllers/api/v1/customers/statements_controller.rb
+++ app/controllers/api/v1/customers/statements_controller.rb
@@ -6,6 +6,37 @@ module Api
       class StatementsController < ApplicationController
 
         def show
+          render json: {
+            message: I18n.t("customer.statement.show.success"),
+            data: {
+              income: customer_incomes,
+              expenditure: customer_expenditures,
+              ie_rating: statement.ie_rating.humanize.titleize,
+              disposable_income_amount: statement.disposable_income_amount
+            }
+          }
+        end
+
+        private
+
+        def statement
+          @statement ||= Statement.find_by(customer: customer)
+        end
+
+        def customer
+          @customer ||= ::Customer.find_by(id: params[:customer_id])
+        end
+
+        def customer_expenditures
+          Expenditure.where(customer: customer).map do |expenditure|
+            expenditure.attributes.slice("category", "amount")
+          end
+        end
+
+        def customer_incomes
+          Income.where(customer: customer).map do |income|
+            income.attributes.slice("source", "amount")
+          end
         end
 
       end
--- spec/requests/api/v1/customers/statement_spec.rb
+++ spec/requests/api/v1/customers/statement_spec.rb
@@ -3,33 +3,56 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Customers::Statement", type: :request do
-  let(:income) { create(:income, source: :salary, amount: 2800.0, customer: customer) }
-  let(:expenditure) { create(:expenditure, category: :mortgage, amount: 500.0, customer: customer) }
-  let(:customer) { create(:customer) }
+  let(:customer) { create(:customer, :with_statement) }
 
   describe "GET /api/v1/customers/:customer_id/statement" do
-    context "when customer has income and expenditure records" do
-      before do
-        income
-        expenditure
-      end
-
+    context "when customer has no income or expenditure records" do
       it "returns http ok" do
-        get "/api/v1/customers/#{customer.id}/statements"
+        get "/api/v1/customers/#{customer.id}/statement", as: :json
 
         expect(response).to have_http_status(:ok)
         expect(response.parsed_body).to eq(
           {
-            "message" => "Statement created successfully",
+            "message" => "Statement fetched successfully",
+            "data" => {
+              "income" => [],
+              "expenditure" => [],
+              "ie_rating" => "Not Calculated",
+              "disposable_income_amount" => "9.99"
+            }
+          }
+        )
+      end
+    end
+
+    context "when customer has income and expenditure records" do
+      before do
+        post(
+          "/api/v1/customers/#{customer.id}/expenditure",
+          params: { expenditure: { category: "mortgage", amount: 500.0 } }
+        )
+        post(
+          "/api/v1/customers/#{customer.id}/income",
+          params: { income: { source: "salary", amount: 2800.0 } }
+        )
+      end
+
+      it "returns http ok", :sidekiq_inline do
+        get "/api/v1/customers/#{customer.id}/statement"
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to eq(
+          {
+            "message" => "Statement fetched successfully",
             "data" => {
               "income" => [
-                { "source" => "Salary", "amount" => 2800.0 }
+                { "source" => "salary", "amount" => "2800.0" }
               ],
               "expenditure" => [
-                { "category" => "Mortgage", "amount" => 500.0 }
+                { "category" => "mortgage", "amount" => "500.0" }
               ],
-              "ie_rating" => "B",
-              "disposable_income" => 2300.0
+              "ie_rating" => "Rated B",
+              "disposable_income_amount" => "2300.0"
             }
           }
         )
